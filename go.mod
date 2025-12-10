f25d277a9d08dedb65e91b8603d1682b2e4d2928
karthick <karthick@example.com>
2025-12-10 19:21:34 +0530

    chore: init go module and fix test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karthick180887/advanced-go
+
+go 1.25.4
--- main_test.go
+++ main_test.go
@@ -1,12 +1,13 @@
 package main
+
 import (
 	"testing"
 )
 
-func TestMain(t *testing.T){
-	t.Run("Process Truck", func(t * testing.T) {
-		t.Run("should load and unload a truck cargo", func(t * testing.T) {
-
-		}
-	}
+func TestMain(t *testing.T) {
+	t.Run("Process Truck", func(t *testing.T) {
+		t.Run("should load and unload a truck cargo", func(t *testing.T) {
+			// TODO: Implement test assertions for truck processing
+		})
+	})
 }
